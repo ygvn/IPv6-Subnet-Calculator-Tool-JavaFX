0f800e7fa5f29b73956a3e21f8e0b46cf2731e93
Yucel Guven <>
2018-12-08 23:38:36 +0200

    v3.2.1 - Commit: 20181209 - Yucel Guven

--- src/ipv6subnettingtool/ListAssignedfromDB.java
+++ src/ipv6subnettingtool/ListAssignedfromDB.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018, Yücel Güven
+ * Copyright (c) 2010-2019, Yucel Guven
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
